2e61cf55cea99d97821f620c3f51c442d8c0e34f
mishymishy5 <michelle.ktaike@gmail.com>
2024-09-24 00:49:29 -0500

    push

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(".myImg").on("click", function() {
+    $(".myImg").animate({ width: "500px"});
+});
+
+
+$(".accordian").on("click", function(){
+    // $(".toggleMe").toggle();
+    $(".toggleMe").slideToggle();
+});
+
